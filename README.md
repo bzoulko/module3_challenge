96f22becc9bb83333e43f3c9e67d43d57f7cbb3b
BrianZoulko <bzoulko@gmail.com>
2022-08-31 08:02:25 -0700

    Updating module challenge assignment.

--- README.md
+++ README.md
@@ -1,2 +1,34 @@
-# module3_challenge
-Password Generator
+# Module 03 Challenge Project - Password Generator
+This program allows the end user to click the "Generate Password" button and 
+produce a randomly generated password based on key elements included based on
+user response to several prompts.
+
+
+## Key elements to notice
+* Prompt user to enter a value between 8 and 128 characters for the generated
+  password length. If an invalid length is supplied, an ALERT message will be
+  displayed. A valid length must be supplied or the user can CANCEL the 
+  password request.
+
+* Prompt user to include at least one of the following character sets:
+    Special Characters - '!@#$%^&*()-+'
+    Uppercase Characters - 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    Lowercase Characters - 'abcdefghijklmnopqrstuvwxyz0123456789'
+    Numbers - '0123456789'
+
+    ALERT message will be displayed if no character set is included.
+
+
+### Spoecial Notes:
+1. Modified application supplied to us as part of module 3 challenge in this bootcamp.
+
+2. Included "reset.css" to this project.
+
+3. Changed the following to meet requirment image example:
+    h1 - Increased font size, set font color to black and made bolder.
+    h2 - Increased font size, set font color to black and made bolder.
+    btn - Rounded the edges of the button.
+
+4. Wrote several functions to produce the desired results.
+
+5. Commented modules to help with future enhancements.
--- assest/js/script.js
+++ assest/js/script.js
@@ -1,4 +1,74 @@
 // Assignment code here
+const cMinLength  = 8;
+const cMaxLength  = 128;
+
+/* ***********************************************************
+  Generate password using characters based on users selection.
+************************************************************** */
+function generatePassword() {
+
+  // ABORT:  When no proper length is selected. 
+  var passLength = getPassLength();
+  if (passLength === null) return ("No password was generated.");
+
+
+  // Make sure at least one character set is selected.
+  var firstTime = true;
+  var charSet = '';  
+  do {
+    if (!firstTime) alert("You must select at least one character set to be included.");
+    firstTime = false;
+  } while ((charSet = getCharacterSet()) == '');
+
+
+  // Randomly select characters from the character set until
+  // the password size is reached.
+  // ************************************************************
+  // Special Notes:
+  //    Larger the factor, the more time it takes to generate 
+  //    a password, but it will generate a more complex password. 
+  // ************************************************************
+  var factor = passLength * charSet.length;
+  var pwd = '';
+  do {
+    var idx = Math.floor(Math.random() * factor);
+    pwd += charSet.charAt(idx);
+  } while (pwd.length != passLength);
+
+  return pwd;
+}
+
+
+/* **************************************************
+  RETURN: List of character set(s) selected.
+***************************************************** */
+function getCharacterSet() {
+  var chList  = (confirm("Include Special Characters?")   ? '!@#$%^&*()-+' : '');
+  chList     += (confirm("Include Uppercase Characters?") ? 'ABCDEFGHIJKLMNOPQRSTUVWXYZ' : '');
+  chList     += (confirm("Include Lowercase Characters?") ? 'abcdefghijklmnopqrstuvwxyz0123456789' : '');
+  chList     += (confirm("Include Numbers?")              ? '0123456789' : '');
+  return (chList);
+}
+
+
+/* ******************************************************
+  RETURN: Password Length if not valid, null is returned.
+********************************************************* */
+function getPassLength() {
+  var passLen = null;
+  for (; ;) {
+
+    passLen = prompt("Enter a password length, must be between 8 and 128 charcters?", 8);
+    if (passLen === null) return (null);
+
+    if (passLen < cMinLength || passLen > cMaxLength) {
+      alert("Invalid password length, please re-enter.");
+    } else
+      break;
+
+  }
+  return (passLen);
+}
 
 
 // Get references to the #generate element
@@ -6,6 +76,7 @@ var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
+  
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
